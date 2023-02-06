90ad12ba774c9b8daddfdf0d7d168d99686e0c91
andraskovacs <puttamalac@gmail.com>
2023-02-06 21:53:54 +0100

    monomorphize basic bench, just to be safe

--- bench/FPBasic.hs
+++ bench/FPBasic.hs
@@ -7,6 +7,7 @@ module FPBasic (
 import FlatParse.Basic
 import FlatParse.Common.Assorted
 
+ws, open, close, ident, sexp, src :: Parser () ()
 ws      = skipMany $(switch [| case _ of " " -> pure (); "\n" -> pure () |])
 open    = $(char '(') >> ws
 close   = $(char ')') >> ws
@@ -15,10 +16,12 @@ sexp    = branch open (skipSome sexp >> close) ident
 src     = sexp >> eof
 runSexp = runParser src
 
+longw, longws :: Parser () ()
 longw     = $(string "thisisalongkeyword")
 longws    = skipSome (longw >> ws) >> eof
 runLongws = runParser longws
 
+numeral, comma, numcsv :: Parser () ()
 numeral   = skipSome (skipSatisfyAscii isDigit) >> ws
 comma     = $(char ',') >> ws
 numcsv    = numeral >> skipMany (comma >> numeral) >> eof
