086d7f68f4d96aab426ac3e7e07cb7b28234590d
Ben Orchard <thefirstmuffinman@gmail.com>
2024-03-15 17:24:17 +0000

    fix Nix build

--- flake.nix
+++ flake.nix
@@ -1,26 +1,47 @@
 {
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    #flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
+  outputs = inputs:
+  let
+    # simple devshell for non-dev compilers: really just want `cabal repl`
+    nondevDevShell = compiler: {
+      mkShellArgs.name = "${compiler}-flatparse";
+      hoogle = false;
+      tools = _: {
+        hlint = null;
+        haskell-language-server = null;
+        ghcid = null;
+      };
+    };
+  in
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
-      perSystem = { self', pkgs, ... }: {
-        haskellProjects.default = {
-          haskellPackages = pkgs.haskell.packages.ghc944;
-          #haskellPackages = pkgs.haskell.packages.ghc925;
-          #haskellPackages = pkgs.haskell.packages.ghc810;
-          packages = {
-            flatparse.root = ./.;
+      perSystem = { self', pkgs, config, ... }: {
+        packages.default  = self'.packages.ghc96-flatparse;
+        devShells.default = self'.devShells.ghc96;
+        haskellProjects.ghc98 = {
+          # shouldn't work, pkgs aren't up to date and mine aren't 9.8 ready
+          basePackages = pkgs.haskell.packages.ghc98;
+          devShell = nondevDevShell "ghc98";
+        };
+        haskellProjects.ghc96 = {
+          basePackages = pkgs.haskell.packages.ghc96;
+          devShell.mkShellArgs.name = "ghc96-flatparse";
+          devShell.tools = _: {
+            haskell-language-server = null; # 2024-03-06: broken
           };
-          # 2023-01-12 raehik: ghcid seems broken on nixpkgs GHC 9.4.{3,4}
-          buildTools = hp: { ghcid = null; hls = null; };
-          # overrides = self: super: { }
-          # hlintCheck.enable = true;
-          # hlsCheck.enable = true;
+        };
+        haskellProjects.ghc94 = {
+          basePackages = pkgs.haskell.packages.ghc94;
+          devShell = nondevDevShell "ghc94";
+        };
+        haskellProjects.ghc92 = {
+          basePackages = pkgs.haskell.packages.ghc92;
+          devShell = nondevDevShell "ghc92";
         };
       };
     };
